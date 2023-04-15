b3ca713fb77cc539761d29f217eb337c5e84eeca
Daniel Palmer <me@0x0f.com>
2023-04-15 11:57:18 +0900

    Initial commit

---
+++ include/dgputil.h
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ *
+ */
+#ifndef __DGPUTIL_H__
+#define __DGPUTIL_H__
+
+/* Very basic level based logging .. */
+
+#define LOGLEVEL_ERR	0
+#define LOGLEVEL_INFO	1
+#define LOGLEVEL_DBG	2
+
+typedef int(*log_cb)(int level, const char *tag, const char *restrict format,...);
+
+/* Very shitty min/max */
+
+#define min(a,b) (((a)<(b))?(a):(b))
+#define max(a,b) (((a)>(b))?(a):(b))
+
+#define array_size(a) (sizeof(a) / sizeof(a[0]))
+
+#define err_ptr(e) ((void *) e)
+#define ptr_err(p) ((int) p)
+
+// fixme!!
+#define is_err_ptr(p)	(((int) p) < 0xff)
+
+#define bit(_b) (1 << _b)
+
+#define flex_array_sz(_a, _fa, _e) \
+	sizeof(*_a) + (sizeof(_a->_fa[0]) * _e)
+
+#endif
